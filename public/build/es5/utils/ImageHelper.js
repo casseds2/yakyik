106511e5f3b465eb92d7ed0f9d622a220130e262
Stephen Cassedy <casseds95@gmail.com>
2017-08-22 22:58:22 +0100

    Removed Public Directory Contents

--- public/build/es5/utils/ImageHelper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-
-    thumbnail: function (url, dimension) {
-        var thumbParams = "upload/c_thumb,h_" + dimension + ",w_" + dimension + ",x_0,y_0";
-        return url.replace("upload", thumbParams);
-    }
-};
